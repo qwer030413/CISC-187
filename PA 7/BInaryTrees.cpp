8ce4b7a3c70b0e4e5385d15ac0145be9210cf20d
Chris <comosjay@gmail.com>
2024-05-09 16:13:10 -0700

    done

--- PA 7/BInaryTrees.cpp
+++ PA 7/BInaryTrees.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <algorithm>
-#include <queue>
 
 using namespace std;
 class IntBinaryTree
@@ -22,8 +21,7 @@ class IntBinaryTree
         int maxWidthHelper(TreeNode *&);
         int getWidth(TreeNode *&, int);
         void destroySubTree(TreeNode *);
-        // void destroySubTree(TreeNode *);
-        // void deleteNode(int ,TreeNode *&);
+      
 
 
     public:
@@ -116,33 +114,19 @@ int main(){
         }
     }
     
-    // node.insertNode(10);
-    // node.insertNode(87);
-    // node.insertNode(9);
-    // node.insertNode(55);
-    // node.insertNode(13);
-    // node.insertNode(40);
-    // node.insertNode(22);
-    // node.insertNode(1);
-    // node.insertNode(0);
-    // node.insertNode(77);
-    // node.insertNode(0);
-    // node.insertNode(4);
-    // node.insertNode(55);
-    // node.insertNode(33);
-    // node.insertNode(22);
-    // node.displayTree();
-    // node.treeDepth();
-    // node.leafCount();
-    // node.maxWidth();
+    
 }
 
+
+//insert function helper, insert new node into binary tree
 void IntBinaryTree::insert(TreeNode *&NodePtr, TreeNode *&newNode)
 {
+    //if tree is empty, add the new node 
     if (NodePtr == nullptr)
     {
         NodePtr = newNode;
     }
+    //if binary tree is not empty, compare values and insert the value in tree accordingly.
     else if (newNode-> value < NodePtr -> value)
     {
         insert(NodePtr -> left, newNode);
@@ -151,10 +135,12 @@ void IntBinaryTree::insert(TreeNode *&NodePtr, TreeNode *&newNode)
         insert(NodePtr -> right, newNode);
     }
 }
+
+//inserting function 
 void IntBinaryTree::insertNode(int num)
 {
     TreeNode *newNode = nullptr;
-
+    //set the value of the newNode to the int number that we got in the parameter and call the inserr function
     newNode = new TreeNode;
     newNode -> value = num;
     newNode -> left = newNode-> right = nullptr;
@@ -162,30 +148,37 @@ void IntBinaryTree::insertNode(int num)
 
 }
 
+//displaying tree
 void IntBinaryTree::displayTree(){
     TreeNode *temp = root;
     displayHelper(temp);
 }
+//displaying tree using recurison
 void IntBinaryTree::displayHelper(TreeNode *&NodePtr){
     if(NodePtr == nullptr)
     {
-        
         return;
     }
-    cout <<NodePtr->value << " "<< endl;
+    //check left
     if(NodePtr->left)
     {
         displayHelper(NodePtr->left);
     }
+    //print our the value of the node
+    cout <<NodePtr->value << " "<< endl;
+    //check right
     if(NodePtr->right)
     {
         displayHelper(NodePtr->right);
     }
 }
+
+//get the maximum tree depth 
 void IntBinaryTree::treeDepth(){
     TreeNode *temp = root;
     cout <<"Tree depth: " <<countHelper(temp, 0) - 1<< endl;
 }
+//helper function for depth, use recursion to get the max depth.
 int IntBinaryTree::countHelper(TreeNode *&NodePtr, int n){
     if(NodePtr == nullptr)
     {
@@ -193,12 +186,13 @@ int IntBinaryTree::countHelper(TreeNode *&NodePtr, int n){
     }
     return max(countHelper(NodePtr->left, n + 1), countHelper(NodePtr->right, n + 1));
 }
-
+//get how many leaves are in the tree
 void IntBinaryTree::leafCount(){
     TreeNode *temp = root;
     cout << "Number of leafs: " << leafHelper(temp) << endl;
 }
 
+//use recursion to transverse through tree until we find a node that left and right is null(a leaf)
 int IntBinaryTree::leafHelper(TreeNode *&NodePtr){
     if(NodePtr == nullptr){
         return 0;
@@ -212,11 +206,13 @@ int IntBinaryTree::leafHelper(TreeNode *&NodePtr){
 
 }
 
+//get the max width of tree
 void IntBinaryTree::maxWidth(){
     TreeNode *temp = root;
     cout << "Max Width: " << maxWidthHelper(temp) << endl;
 }
 
+//helper function that compares the width of the tree for each level/index and gets the max
 int IntBinaryTree::maxWidthHelper(TreeNode *&NodePtr)
 {
     int maxWidth = 0;
@@ -229,6 +225,7 @@ int IntBinaryTree::maxWidthHelper(TreeNode *&NodePtr)
  
     return maxWidth;
 }
+//getting the width of each level/index of tree
 int IntBinaryTree::getWidth(TreeNode *&root, int index)
 {
     if (root == nullptr)
@@ -239,9 +236,7 @@ int IntBinaryTree::getWidth(TreeNode *&root, int index)
         return getWidth(root->left, index - 1) + getWidth(root->right, index - 1);
 }
 
-// IntBinaryTree::~IntBinaryTree(){
-//     destroySubTree(root);
-// }
+//destructor helper
 void IntBinaryTree::destroySubTree(TreeNode* temp)
 {
     if (temp)
