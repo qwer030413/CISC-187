d10f88b3a0a259de80d06c18a55c97d1bd9bf310
Chris <comosjay@gmail.com>
2024-05-09 14:59:10 -0700

    aaa

--- PA 7/BInaryTrees.cpp
+++ PA 7/BInaryTrees.cpp
@@ -21,6 +21,7 @@ class IntBinaryTree
         int leafHelper(TreeNode *&);
         int maxWidthHelper(TreeNode *&);
         int getWidth(TreeNode *&, int);
+        void destroySubTree(TreeNode *);
         // void destroySubTree(TreeNode *);
         // void deleteNode(int ,TreeNode *&);
 
@@ -29,17 +30,16 @@ class IntBinaryTree
         IntBinaryTree() {
             root = nullptr;
         }
-        // ~IntBinaryTree()
-        // {
-        //     destroySubTree(root)
-        // }
+        ~IntBinaryTree()
+        {
+            destroySubTree(root);
+        }
         void insertNode(int);
         void displayTree();
         void treeDepth();
         void leafCount();
         void maxWidth();
-        // bool searchNode(int);
-        // void remove(int);
+        
 };
 
 
@@ -47,25 +47,94 @@ int main(){
 
 
     IntBinaryTree node;
-    node.insertNode(10);
-    node.insertNode(87);
-    node.insertNode(9);
-    node.insertNode(55);
-    node.insertNode(13);
-    node.insertNode(40);
-    node.insertNode(22);
-    node.insertNode(1);
-    node.insertNode(0);
-    node.insertNode(77);
-    node.insertNode(0);
-    node.insertNode(4);
-    node.insertNode(55);
-    node.insertNode(33);
-    node.insertNode(22);
-    node.displayTree();
-    node.treeDepth();
-    node.leafCount();
-    node.maxWidth();
+    int input = 0;
+    while (input !=6)
+    {
+        cout << "Select an option: \n";
+        cout << "1. Insert one integer into the tree \n";
+        cout << "2. display the tree \n";
+        cout << "3. Display Leaf Count \n";
+        cout << "4. Display Tree Height \n";
+        cout << "5. Display Tree Width \n";
+        cout << "6. Exit \n";
+        cout << "Enter your choice: ";
+        cin >> input;
+        //if input value is invalid and not an int, tell them the input is invalid and prompt them for a new option
+        while(cin.fail()){
+            cout << "Error, invalid input \n";
+            cin.clear();
+            cin.ignore(256, '\n');
+            cout << "Select an option: \n";
+            cout << "1. Insert one integer into the tree \n";
+            cout << "2. display the tree \n";
+            cout << "3. Display Leaf Count \n";
+            cout << "4. Display Tree Height \n";
+            cout << "5. Display Tree Width \n";
+            cout << "6. Exit \n";
+            cout << "Enter your choice: ";
+            cin >> input;
+        }
+        switch (input)
+        {
+            //insert number into tree
+            case 1:{
+                int newNode;
+                cout << "Enter number: ";
+                cin >> newNode;
+                node.insertNode(newNode);
+                break;
+            }
+            //display tree
+            case 2:{
+                node.displayTree();
+                break;
+            }
+            //print out how many leafs are in the tree
+            case 3:{
+                node.leafCount();
+                break;
+            }
+            //print out the maximum depth of the tree
+            case 4:{
+                node.treeDepth();
+                break;
+            }
+            //display the maximum width of the tree
+            case 5:{
+                node.maxWidth();
+                break;
+            }
+            //exit program
+            case 6:{
+                cout<< "Exiting program....";
+                break;
+            }
+            //if input is int value but not on menu, tell user their input is invalid
+            default:{
+                cout << "Invalid input \n";
+            }
+        }
+    }
+    
+    // node.insertNode(10);
+    // node.insertNode(87);
+    // node.insertNode(9);
+    // node.insertNode(55);
+    // node.insertNode(13);
+    // node.insertNode(40);
+    // node.insertNode(22);
+    // node.insertNode(1);
+    // node.insertNode(0);
+    // node.insertNode(77);
+    // node.insertNode(0);
+    // node.insertNode(4);
+    // node.insertNode(55);
+    // node.insertNode(33);
+    // node.insertNode(22);
+    // node.displayTree();
+    // node.treeDepth();
+    // node.leafCount();
+    // node.maxWidth();
 }
 
 void IntBinaryTree::insert(TreeNode *&NodePtr, TreeNode *&newNode)
@@ -100,9 +169,10 @@ void IntBinaryTree::displayTree(){
 void IntBinaryTree::displayHelper(TreeNode *&NodePtr){
     if(NodePtr == nullptr)
     {
+        
         return;
     }
-    cout <<NodePtr->value << " ";
+    cout <<NodePtr->value << " "<< endl;
     if(NodePtr->left)
     {
         displayHelper(NodePtr->left);
@@ -152,23 +222,38 @@ int IntBinaryTree::maxWidthHelper(TreeNode *&NodePtr)
     int maxWidth = 0;
     int width = 0;
     int height = countHelper(root, 0);
- 
-    /* Get width of each level and compare
-        the width with maximum width so far */
     for (int i = 1; i <= height; i++) {
         width = getWidth(root, i);
-        if (width > maxWidth)
-            maxWidth = width;
+        maxWidth = max(width, maxWidth);
     }
  
     return maxWidth;
 }
-int IntBinaryTree::getWidth(TreeNode *&root, int level)
+int IntBinaryTree::getWidth(TreeNode *&root, int index)
 {
     if (root == nullptr)
         return 0;
-    if (level == 1)
+    if (index == 1)
         return 1;
-    else if (level > 1)
-        return getWidth(root->left, level - 1) + getWidth(root->right, level - 1);
+    else if (index > 1)
+        return getWidth(root->left, index - 1) + getWidth(root->right, index - 1);
+}
+
+// IntBinaryTree::~IntBinaryTree(){
+//     destroySubTree(root);
+// }
+void IntBinaryTree::destroySubTree(TreeNode* temp)
+{
+    if (temp)
+    {
+        if(temp -> left)
+        {
+            destroySubTree(temp->left);
+        }
+        if(temp -> right)
+        {
+            destroySubTree(temp->right);
+        }
+        delete temp;
+    }
 }
--- PA 7/tempCodeRunnerFile.cpp
+++ PA 7/tempCodeRunnerFile.cpp
@@ -1,2 +1,15 @@
 
-    // displayHelper(temp);
+                node.insertNode(87);
+                node.insertNode(9);
+                node.insertNode(55);
+                node.insertNode(13);
+                node.insertNode(40);
+                node.insertNode(22);
+                node.insertNode(1);
+                node.insertNode(0);
+                node.insertNode(77);
+                node.insertNode(0);
+                node.insertNode(4);
+                node.insertNode(55);
+                node.insertNode(33);
+                node.insertNode(22);
